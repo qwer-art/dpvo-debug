56ce1e24d013132db09d8a6cb01cbc213677343c
Unknown <1062428799@qq.com>
2025-11-22 17:47:49 +0800

    初步跑通代码，准备debug

---
+++ demo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ $1=="debug" ]]; then
+    export PYDEBUG=" -m debugpy --wait-for-client --listen localhost:3197 "
+    shift
+else
+    export PYDEBUG=""
+fi
+
+python_file=/home/jerett/Project/DPVO/demo.py
+project_path=/home/jerett/Project/DPVO
+python $PYDEBUG "$python_file" "$@" --imagedir=$project_path/movies/IMG_0492.MOV --calib=$project_path/calib/iphone.txt --stride=5 --save_ply
